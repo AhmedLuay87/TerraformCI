8a2e84beefe690152772de44d90377f1f5a99ef7
Ahmed Luay <ahmed.luay@nordcloud.com>
2023-05-11 11:33:06 +0200

    testing123

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create resource type storage account
 resource "azurerm_storage_account" "storage" {
-  name                     = "tfsatest5096844tf123"
+  name                     = "tfsatest5096844tf1212"
   location                 = "west europe"
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
